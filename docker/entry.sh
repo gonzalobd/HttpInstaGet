e2c958d74f1414f210e3d5c77eaaf168bf97d4c6
hadoop <gonzalobd>
2017-04-23 14:30:42 +0200

    editing dockerfile

--- docker/entry.sh
+++ docker/entry.sh
@@ -4,6 +4,7 @@ cd /opt/kafka_$SCALA_VERSION-$KAFKA_VERSION
 echo advertised.listeners=PLAINTEXT://$HOST:9092 >> config/server.properties
 bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 bin/kafka-server-start.sh  -daemon config/server.properties
+sleep 5
 bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic like \
 --partitions 1 --replication-factor 1
 bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic comment \
