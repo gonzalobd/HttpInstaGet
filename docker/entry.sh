4adc6a6e32b7f1d649e4ea6a1e9510f217fa9d96
hadoop <gonzalobd>
2017-04-23 13:54:06 +0200

    edits on dockerfile

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 cd /opt/kafka_$SCALA_VERSION-$KAFKA_VERSION
+echo advertised.listeners=PLAINTEXT://$HOST:9092 >> config/server.properties
 bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
-bin/kafka-server-start.sh -daemon config/server.properties
+bin/kafka-server-start.sh  -daemon config/server.properties
 
 while true; do sleep 1000; done
 
