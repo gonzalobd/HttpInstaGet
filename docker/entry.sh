c9500732ac84879b212f862605985e276a4028c8
hadoop <gonzalobd>
2017-04-23 13:59:36 +0200

    edits on dockerfile

--- docker/entry.sh
+++ docker/entry.sh
@@ -4,6 +4,10 @@ cd /opt/kafka_$SCALA_VERSION-$KAFKA_VERSION
 echo advertised.listeners=PLAINTEXT://$HOST:9092 >> config/server.properties
 bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 bin/kafka-server-start.sh  -daemon config/server.properties
+bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic like \
+--partitions 1 --replication-factor 1
+bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic comment \
+--partitions 1 --replication-factor 1
 
 while true; do sleep 1000; done
 
